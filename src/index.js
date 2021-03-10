857d894208b0426842b1b530a36ec53e353154b0
Matheus Elyasha <matheuselyasha@gmail.com>
2021-03-10 01:07:38 -0300

    primeiro projeto com nodejs finalizado

--- src/index.js
+++ src/index.js
@@ -1,57 +1,148 @@
-const express = require('express');
+const { request, response } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 
+const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+  return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 /**
- * GET - Buscar informações dentro do servidor
- * POST - Criar um objeto / Inserir uma informação no servidor
- * PUT - Alterar uma informação no servidor
- * PATCH - Alterar uma informação específica no servidor
- * DELETE - Deletar uma informação no servidor
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
  */
 
- /**
-  * Tipos de parâmetros
-  * 
-  * Route Params = Identificar um recurso (editar/deletar/buscar)
-  * Query Params = Paginação / Filtro
-  * Body Params => Objetos para inserção/alteração de um recurso (JSON)
-  */
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
 
-app.get("/", (request, response) => {
-    // return response.send("Hello world!") // Vamos usar poucas vezes o send()
-    // return response.json({ message: "Hello World Ignite - Fundamentos NodeJS!" });
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
 
-    return response.json({ message: "Hello World Ignite!" });
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
 });
 
-app.get("/courses", (request, response) => {   
-    const query = request.query;
-    console.log(query);
-    return response.json(["Curso 1", "Curso 2", "Curso 3"])
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
 });
 
-app.post("/courses", (request, response) => {
-    const body = request.body;
-    console.log(body);
-    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer);
 });
 
-app.put("/courses/:id", (request, response) => {  
-    const params = request.params;
-    console.log(params)
-    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+// app.use(verifyIfExistsAccountCPF); // Se eu quero que todas rotas tenham esse middleware
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  //   const { cpf } = request.params;
+  const { customer } = request;
+  return response.json(customer.statement);
 });
 
-app.patch("/courses/:id", (request, response) => {
-    return response.json(["Curso 8", "Curso 2", "Curso 3", "Curso 4"]);
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
 });
 
-app.delete("/courses/:id", (request, response) => {
-    return response.json(["Curso 8", "Curso 2", "Curso 4"]);
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const {customer} = request;
+
+    const balance = getBalance(customer.statement);
+    return response.json(balance);
 });
 
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const {customer} = request;
+
+    // splice
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
 
 app.listen(3333);
